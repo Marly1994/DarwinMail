b575a61c7ef088275c57e123f984ab6a5938fa66
Marlene lopez <Marlene lopez>
2017-04-19 14:50:32 -0500

    Se crea la interfaz de la aplicacion segun el Mockup

--- Darwin-Mail/app/src/main/java/com/jsalv/darwin_mail/MainActivity_loggin.java
+++ Darwin-Mail/app/src/main/java/com/jsalv/darwin_mail/MainActivity_loggin.java
@@ -1,25 +1,35 @@
 package com.jsalv.darwin_mail;
 
-import android.content.Intent;
+import android.graphics.Typeface;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.view.View;
 import android.widget.Button;
 
 public class MainActivity_loggin extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_loggin);
-        Button boton = (Button)findViewById(R.id.Bot1);
-        boton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent datos = new Intent(getApplicationContext(),datos_admin.class);
-                startActivity(datos);
-            }
-        });
+//        Button boton = (Button)findViewById(R.id.Alumno);
+//        boton.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                Intent datos = new Intent(getApplicationContext(),datos_admin.class);
+//                startActivity(datos);
+//
+//
+//            }
+//        });
+
+
+        Button alumno=(Button)findViewById(R.id.Alumno);
+        Button admin=(Button)findViewById(R.id.Adm);
+        Typeface TF = Typeface.createFromAsset(getAssets(),"Roboto-Medium.ttf");
+        alumno.setTypeface(TF);
+        admin.setTypeface(TF);
+
     }
 
 
